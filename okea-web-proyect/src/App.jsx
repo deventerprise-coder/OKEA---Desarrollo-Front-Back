9e99623c8ee0efce966697da77fe8cf8403cbd2b
MrAaron <aaronponcevi@gmail.com>
2025-08-24 00:22:10 -0500

    Creacion de Carpetas del Proyecto OKEA

---
+++ okea-web-proyect/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <h1>¡Bienvenido a OKEA!</h1>
+            <p>Este es La pagina de Inicio</p>
+        </div>
+    );
+}
+
+export default App;
